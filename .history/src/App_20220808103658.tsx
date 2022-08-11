2b5ddee1ab83fe496d3ae30bc8c7960b4bfd3962
Matthew Ko <iammatthewko114@gmail.com>
2022-08-11 11:56:13 +0800

    init

---
+++ .history/src/App_20220808103658.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Web3AuthCore from "@web3auth/core";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>hahaha</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20220808104330.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Web3AuthCore } from "@web3auth/core";
+import { CHAIN_NAMESPACES } from '@web3auth/base';
+import {Buffer} from 'buffer';
+
+function App() {
+
+  const web3auth = new Web3AuthCore({
+    chainConfig: {
+      chainNamespace: CHAIN_NAMESPACES.EIP155,
+      chainId: "0x1",
+      rpcTarget: "https://rpc.ankr.com/eth",
+    }
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>hahaha</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
